73ae7acf1a1753e9c3653091fecc092572ceba7c
Jose Efraim Aguilar Escamilla <aguilar.jose11@outlook.com>
2019-06-29 18:00:40 -0500

    Setup main files.

---
+++ chronos.py
@@ -0,0 +1,14 @@
+''' 
+This is the entry point. Run this as Main program. Read README.md for more info on this program.
+
+    @author  Jose Efraim Aguilar Escamilla
+    @version v0.01
+    @since   2019-06-29 
+'''
+from ChronosUtil.Menu import ChronosMainMenu
+
+
+
+
+if __name__ == "__main__":
+    main()
