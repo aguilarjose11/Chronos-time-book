dc54b9988da68201f28922f6665621dc533fa5c8
Jose Efraim Aguilar Escamilla <aguilar.jose11@outlook.com>
2020-07-17 04:56:40 -0500

    added travis & tox CI/CD

--- src/chronos/chronos.py
+++ src/chronos/chronos.py
@@ -1,21 +1,6 @@
-''' 
-This is the entry point. Run this as Main program. Read README.md for more info on this program.
-
-    @author  Jose Efraim Aguilar Escamilla
-    @version v0.01
-    @since   2019-06-29 
-'''
-from ChronosUtil.Menu import ChronosMainMenu
-
 
 def main():
-    '''
-Main method. This contains the main loop.
-
-    @author  Jose Efraim Aguilar Escamilla
-    @version v0.1
-    @since   2019-07-08
-'''
+    """Start Chronos main loop."""
     pass
 
 
